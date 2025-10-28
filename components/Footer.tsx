897ac2647d6721be1ad8022f7d83c19dc697f039
khubaixkhan <khubaibxkhan22@gmail.com>
2025-10-28 22:19:22 +0530

    adding footer and logos

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import Container from './Container'
 import FooterTop from './FooterTop'
+import Logo from './Logo'
 
 const Footer = () => {
   return (
+
     <footer>
-      
       <Container className='bg-white border-t'>
         <FooterTop />
+        <div>
+          <div>
+            <Logo />
+          </div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </Container>
     </footer>
+    
   )
 }
 
