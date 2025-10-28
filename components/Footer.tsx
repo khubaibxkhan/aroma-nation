b289c838c42a568e84a248178595c02b17212939
khubaixkhan <khubaibxkhan22@gmail.com>
2025-10-28 22:28:20 +0530

    adding sm links in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,16 +2,25 @@ import React from 'react'
 import Container from './Container'
 import FooterTop from './FooterTop'
 import Logo from './Logo'
+import SocialMedia from './SocialMedia'
 
 const Footer = () => {
   return (
 
-    <footer>
-      <Container className='bg-white border-t'>
+    <footer className='bg-white border-t'>
+      <Container>
         <FooterTop />
         <div>
-          <div>
+          <div className='space-y-4'>
             <Logo />
+
+            <p>
+              Discover a world of captivating aromas with AromaNation.
+            </p>
+            <SocialMedia 
+                className="text-darkColor/60"
+                iconClassName='border-darkColor/60 hover:border-shop_light_green' 
+                          />
           </div>
           <div></div>
           <div></div>
